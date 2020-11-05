530cafb363723b6598881f8ba5eea9ce46c83afc
Edoardo Ruiz <edoardrui0@gmail.com>
2020-11-05 14:44:41 -0500

    git init

---
+++ index.js
@@ -0,0 +1,11 @@
+// Requirements:
+
+// 1) The user must be able to search for a GitHub user handle.
+
+// 2) The search must trigger a call to GitHub's API.
+
+// 3) The repos associated with that handle must be displayed on the page.
+
+// 4) You must display the repo name and link to the repo URL.
+
+// 5) The user must be able to make multiple searches and see only the results for the current search.
